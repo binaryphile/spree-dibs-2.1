9cf3b0935aa23e7813e86f016e94635fdce0ff24
Ted Lilley <tlilley@diditbetter.com>
2014-01-15 16:48:59 -0500

    Remove dibs-referral overrides

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -3,16 +3,9 @@ Spree::OrderMailer.class_eval do
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-    if @order.dibs_referral
-      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email) do |format|
-        format.text
-        format.html
-      end
-    else
-      mail(to: @order.email, from: from_address, subject: subject) do |format|
-        format.text
-        format.html
-      end
+    mail(to: @order.email, from: from_address, subject: subject) do |format|
+      format.text
+      format.html
     end
   end
 
@@ -20,16 +13,9 @@ Spree::OrderMailer.class_eval do
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
-    if @order.dibs_referral
-      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email) do |format|
-        format.text
-        format.html
-      end
-    else
-      mail(to: @order.email, from: from_address, subject: subject) do |format|
-        format.text
-        format.html
-      end
+    mail(to: @order.email, from: from_address, subject: subject) do |format|
+      format.text
+      format.html
     end
   end
 
@@ -37,16 +23,9 @@ Spree::OrderMailer.class_eval do
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.void_email.subject')} ##{@order.number}"
-    if @order.dibs_referral
-      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email) do |format|
-        format.text
-        format.html
-      end
-    else
-      mail(to: @order.email, from: from_address, subject: subject) do |format|
-        format.text
-        format.html
-      end
+    mail(to: @order.email, from: from_address, subject: subject) do |format|
+      format.text
+      format.html
     end
   end
 
@@ -54,16 +33,9 @@ Spree::OrderMailer.class_eval do
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.capture_email.subject')} ##{@order.number}"
-    if @order.dibs_referral
-      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email) do |format|
-        format.text
-        format.html
-      end
-    else
-      mail(to: @order.email, from: from_address, subject: subject) do |format|
-        format.text
-        format.html
-      end
+    mail(to: @order.email, from: from_address, subject: subject) do |format|
+      format.text
+      format.html
     end
   end
 end
