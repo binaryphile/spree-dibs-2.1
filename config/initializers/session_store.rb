68731d387859b571ae29a794b042c5a32dddd682
Ted Lilley <tlilley@diditbetter.com>
2014-01-29 15:45:25 -0500

    Add cookie domain back

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SpreeDibs::Application.config.session_store :cookie_store, key: '_spree-dibs_session'
+SpreeDibs::Application.config.session_store :cookie_store, key: '_spree_dibs_session', domain: 'diditbetter.com'
